a314d6392a81ee488209b5609029e065d844ad92
Edis <eds.sbn@gmail.com>
2023-02-23 13:55:44 +0100

    Service implementation changes

--- src/main/java/com/example/tfr/demo/service/InvoiceService.java
+++ src/main/java/com/example/tfr/demo/service/InvoiceService.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class InvoiceService {
 
     public List<Invoice> createInvoices(List<Product> products) {
+
         List<Invoice> invoices = new ArrayList<>();
 
         while (!products.isEmpty()) {
@@ -23,14 +24,19 @@ public class InvoiceService {
                 BigDecimal productTotal = calculateProductTotal(product);
                 total = total.add(productTotal);
                 if (total.compareTo(new BigDecimal(500)) < 0 || singleInvoiceProducts.isEmpty()) {
-                    singleInvoiceProducts.add(product);
+                    if (product.getQuantity() > 50) {
+                        product.setQuantity(product.getQuantity() - 50);
+                        singleInvoiceProducts.add(new Product(product.getName(), 50, product.getPrice(),
+                                product.getDiscount(), product.getTax(), ""));
+                    } else {
+                        singleInvoiceProducts.add(product);
+                    }
                 } else {
                     total = total.subtract(productTotal);
                 }
             }
             invoices.add(generateInvoice(singleInvoiceProducts));
             products.removeAll(singleInvoiceProducts);
-
         }
 
         return invoices;
@@ -42,10 +48,7 @@ public class InvoiceService {
         BigDecimal invTax = BigDecimal.ZERO;
         BigDecimal invTotal = BigDecimal.ZERO;
 
-        Invoice invoice = new Invoice();
-
         for (Product product : products) {
-
             BigDecimal subtotal = BigDecimal.valueOf(product.getQuantity())
                     .multiply(product.getPrice().subtract(product.getDiscount()));
             BigDecimal tax = subtotal.multiply(BigDecimal.valueOf(product.getTax()).divide(BigDecimal.valueOf(100)));
@@ -62,16 +65,13 @@ public class InvoiceService {
             invTotal = invTotal.add(total);
         }
 
-        invoice.setProducts(products);
-        invoice.setSubtotal(invSubtotal);
-        invoice.setTax(invTax);
-        invoice.setTotal(invTotal);
-
-        return invoice;
+        return new Invoice(products, invSubtotal, invTax, invTotal);
     }
 
     private BigDecimal calculateProductTotal(Product product) {
-        BigDecimal subtotal = BigDecimal.valueOf(product.getQuantity())
+
+        Integer quantity = Math.min(50, product.getQuantity());
+        BigDecimal subtotal = BigDecimal.valueOf(quantity)
                 .multiply(product.getPrice().subtract(product.getDiscount()));
         BigDecimal tax = subtotal.multiply(BigDecimal.valueOf(product.getTax()).divide(BigDecimal.valueOf(100)));
         return subtotal.add(tax);
